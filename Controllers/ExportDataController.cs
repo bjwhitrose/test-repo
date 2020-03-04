7548b8098b3eb2a8f14ca8decd0b03420beddad2
bjwhitrose <bjwhitrose@gmail.com>
2020-03-04 16:40:51 -0500

    Add files via upload

---
+++ Controllers/ExportDataController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PlanningSystem.Data;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PlanningSystem.Controllers
+{
+    public partial class ExportDataController : ExportController
+    {
+        private readonly POAPP001Context context;
+
+        public ExportDataController(POAPP001Context context)
+        {
+            this.context = context;
+        }
+        
+        [HttpGet("/export/data/Dac.AccountResult/excel")]
+        public FileStreamResult ExportCustomersToExcel()
+        {
+
+            return ToExcel(ApplyQuery(context.CustAssessResultTable, Request.Query));
+        }
+        [HttpGet("/export/data/CustAssessResultTable/excel")]
+        public FileStreamResult ExportCustomers()
+        {
+
+            return ToExcel(ApplyQuery(context.CustAssessResultTable, Request.Query));
+        }
+    }
+}
